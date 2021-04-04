f876bcd756cdebfa9f245efdd516957c075d8232
kazuki-komori <komokazu.nankai@gmail.com>
2021-04-04 13:20:03 +0000

    add: マイグレーションファイル

---
+++ postgres/schemes/20210404111419-contents.sql
@@ -0,0 +1,13 @@
+
+-- +migrate Up
+CREATE TABLE contents (
+    id SERIAL,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    user_id VARCHAR(100) REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    subject_id VARCHAR(50) NOT NULL ,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+-- +migrate Down
+DROP TABLE IF EXISTS contents;
